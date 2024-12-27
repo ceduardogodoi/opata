8259e58f3e6c97a90601b887ec061ef6b44720a1
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2024-12-27 16:08:23 -0300

    fix: ci

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 
 const environmentVariables = EnvironmentVariables.getInstance();
 const [error] = environmentVariables.validateEnv();
+console.log("error::", error);
 if (error != null) {
   // Interrupts Next.js app bootstrapping
   throw error;
