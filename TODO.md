e81cf3e355eefb6dec3a09d78080fefe9760083a
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2025-01-03 22:08:31 -0300

    feat: get animals pagination

--- TODO.md
+++ TODO.md
@@ -12,11 +12,12 @@
 - [X] Create `preview` environment to GitHub repository
   - [X] Update `preview.yml` to use `preview` environment
   - [X] Delete `development` environment from GitHub repository
-- [WIP #2] Configure to only deploy to vercel after GitHub Actions ran successfully
-- [ ] Create pagination to `GET /animals
+- [X] Create pagination to `GET /animals
 - [ ] Create filter to `GET /animals`
 - [ ] Create get animal by id `GET /animals/:id`
 
 - [ ] Improve `README.md`
 - [ ] Add linting to CI
 - [ ] Update vercel envs with GitHub Actions envs (vercel CLI: `vercel env`)
+- [ ] Configure to only deploy to vercel after GitHub Actions ran successfully
+- [ ] Add problem+json error
--- src/app/infra/http/routes/find-all-animals/find-all-animals.route.spec.ts
+++ src/app/infra/http/routes/find-all-animals/find-all-animals.route.spec.ts
@@ -2,7 +2,6 @@ import { beforeEach, describe, expect, it } from "vitest";
 import { container } from "tsyringe";
 import { AnimalRepositoryGateway } from "@/app/domain/animal/gateway/animal-repository.gateway.interface";
 import { FindAllAnimalsUseCase } from "@/app/use-cases/find-all-animals/find-all-animals.usecase";
-import { FindAllAnimalsPresentOutput } from "../../presenters/find-all-animals/find-all-animals.presenter.dto";
 import { FindAllAnimalsRoute } from "./find-all-animals.route";
 import { animalInstanceFixture } from "@/app/fixtures/animal.fixtures";
 
@@ -17,20 +16,106 @@ describe("routes / find all animals", () => {
   });
 
   it("should return 404 when there are no registered animals", async () => {
-    const response = await findAllAnimalsRoute.handle();
-    const output: FindAllAnimalsPresentOutput = await response.json();
+    const request = {
+      url: "http://localhost:3000/api/animals",
+    } as Request;
+
+    const response = await findAllAnimalsRoute.handle(request);
+    const output = await response.json();
+
     expect(response.status).toBe(404);
-    expect(output).toHaveLength(0);
+    expect(output.items).toHaveLength(0);
+    expect(output.totalItems).toBe(0);
+    expect(output.totalPages).toBe(0);
+    expect(output.currentPage).toBe(0);
   });
 
-  it("should return 200 with all registered animals", async () => {
+  it("should return 200 with all 3 registered animals", async () => {
+    const request = {
+      url: "http://localhost:3000/api/animals",
+    } as Request;
+
     Array.from({ length: 3 }).forEach(async () => {
       await animalRepository.save(animalInstanceFixture);
     });
 
-    const response = await findAllAnimalsRoute.handle();
-    const output: FindAllAnimalsPresentOutput = await response.json();
+    const response = await findAllAnimalsRoute.handle(request);
+    const output = await response.json();
+
     expect(response.status).toBe(200);
-    expect(output).toHaveLength(3);
+    expect(output.items).toHaveLength(3);
+    expect(output.totalItems).toBe(3);
+    expect(output.totalPages).toBe(1);
+    expect(output.currentPage).toBe(1);
+  });
+
+  it("should return 200 with 10 animals from all 20 registered animals", async () => {
+    const request = {
+      url: "http://localhost:3000/api/animals",
+    } as Request;
+
+    Array.from({ length: 17 }).forEach(async () => {
+      await animalRepository.save(animalInstanceFixture);
+    });
+
+    const response = await findAllAnimalsRoute.handle(request);
+    const output = await response.json();
+
+    expect(response.status).toBe(200);
+    expect(output.items).toHaveLength(10);
+    expect(output.totalItems).toBe(20);
+    expect(output.totalPages).toBe(2);
+    expect(output.currentPage).toBe(1);
+  });
+
+  it("should return 200 with default settings when both search params are invalid", async () => {
+    const request = {
+      url: "http://localhost:3000/api/animals?page=x&pageSize=y",
+    } as Request;
+
+    const response = await findAllAnimalsRoute.handle(request);
+    const output = await response.json();
+
+    expect(response.status).toBe(200);
+    expect(output.items).toHaveLength(10);
+    expect(output.totalItems).toBe(20);
+    expect(output.totalPages).toBe(2);
+    expect(output.currentPage).toBe(1);
+  });
+
+  it.each([
+    { searchParam: "page", value: "x" },
+    { searchParam: "pageSize", value: "y" },
+  ])(
+    "should return 200 with default settings when $searchParam search param is invalid",
+    async ({ searchParam, value }) => {
+      const request = {
+        url: `http://localhost:3000/api/animals?${searchParam}=${value}`,
+      } as Request;
+
+      const response = await findAllAnimalsRoute.handle(request);
+      const output = await response.json();
+
+      expect(response.status).toBe(200);
+      expect(output.items).toHaveLength(10);
+      expect(output.totalItems).toBe(20);
+      expect(output.totalPages).toBe(2);
+      expect(output.currentPage).toBe(1);
+    }
+  );
+
+  it("should return 200 with customized settings when both search params are valid", async () => {
+    const request = {
+      url: "http://localhost:3000/api/animals?page=2&pageSize=5",
+    } as Request;
+
+    const response = await findAllAnimalsRoute.handle(request);
+    const output = await response.json();
+
+    expect(response.status).toBe(200);
+    expect(output.items).toHaveLength(5);
+    expect(output.totalItems).toBe(20);
+    expect(output.totalPages).toBe(4);
+    expect(output.currentPage).toBe(2);
   });
 });
