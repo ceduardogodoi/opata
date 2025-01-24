64fcbdbb1fdf3e9ba22095096103f0e791aba284
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2025-01-23 21:44:20 -0300

    feat: pluralize folders

--- src/app/api/animals/route.ts
+++ src/app/api/animals/route.ts
@@ -2,9 +2,9 @@
 import "@/app/infra/container";
 
 import { container } from "tsyringe";
-import { CreateAnimalRoute } from "@/app/infra/http/routes/animal/create-animal/create-animal.route";
+import { CreateAnimalRoute } from "@/app/infra/http/routes/animals/create-animal/create-animal.route";
 import { AnimalRepositoryGateway } from "@/app/domain/animal/gateway/animal-repository.gateway.interface";
-import { FindAllAnimalsRoute } from "@/app/infra/http/routes/animal/find-all-animals/find-all-animals.route";
+import { FindAllAnimalsRoute } from "@/app/infra/http/routes/animals/find-all-animals/find-all-animals.route";
 import { CreateAnimalUseCase } from "@/app/use-cases/animals/create-animal/create-animal.usecase";
 import { FindAllAnimalsUseCase } from "@/app/use-cases/animals/find-all-animals/find-all-animals.usecase";
 
